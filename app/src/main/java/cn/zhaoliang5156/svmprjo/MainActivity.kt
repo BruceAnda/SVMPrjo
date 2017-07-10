8cd811b60858bb3ce5f6e7ccec35508ce036577d
zhaoliang <2668645098@qq.com>
2017-07-10 16:19:16 +0800

    Initial commit

---
+++ app/src/main/java/cn/zhaoliang5156/svmprjo/MainActivity.kt
@@ -0,0 +1,12 @@
+package cn.zhaoliang5156.svmprjo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
